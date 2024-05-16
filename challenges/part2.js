a0d033f16875451faf55ecf7581fc99365f33612
Adil Raqioui <araqioui@e1r6p9.1337.ma>
2024-05-16 14:49:27 +0100

    DONE: Part2 of the course exercises

--- challenges/part2.js
+++ challenges/part2.js
@@ -1,11 +1,38 @@
-let post = '';
+let post;
 
 const jsInput = document.querySelector('input');
 const jsDiv = document.querySelector('div');
+const jsBtn = document.querySelector('button');
 
 function handleInput() {
   post = jsInput.value;
   jsDiv.textContent = post;
 }
 
+function handleClick() {
+  jsInput.value = '';
+}
+
+// Extension Challenge: Submit Button
+function handleClickBtn() {
+  if (jsInput.value) {
+    const newDiv = document.createElement('div');
+    newDiv.textContent = jsDiv.textContent;
+    jsDiv.textContent = jsInput.value;
+    document.body.appendChild(newDiv);
+  }
+}
+
+function dataToView() {
+  if (post === undefined) {
+    post = "What's on your mind?";
+  }
+  jsInput.value = post;
+  jsDiv.textContent = jsInput.value;
+}
+
 jsInput.oninput = handleInput;
+jsInput.onclick = handleClick;
+jsBtn.onclick = handleClickBtn;
+
+setInterval(dataToView, 15);
