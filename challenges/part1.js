1c854b0e6a43ddfa5f1fa10875c712d327496a9a
Adil Raqioui <araqioui@e1r6p9.1337.ma>
2024-05-16 11:59:12 +0100

    DONE: Part1 of the course exercises

--- challenges/part1.js
+++ challenges/part1.js
@@ -1,1 +1,19 @@
 /** Write your code below */
+
+// Variable to store data form the input element
+let post = '';
+
+// Object that will store input node
+const jsInput = document.querySelector('input');
+
+// Object that will store div node
+const jsDiv = document.querySelector('div');
+
+// function that will called at each change at the input
+function handleInput() {
+    post = jsInput.value;
+    jsDiv.textContent = post;    
+}
+
+// Add the handleInput function as a callback function at the oninput event
+jsInput.oninput = handleInput;
